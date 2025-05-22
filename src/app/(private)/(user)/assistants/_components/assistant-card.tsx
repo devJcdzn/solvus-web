76c2eb9489135bb8128c28144519f3f226e63c72
Jean Carlos <lopesjean81@gmail.com>
2025-05-22 08:52:26 -0300

    update AssistantCard component with hover effect and add new ContactPage component

--- src/app/(private)/(user)/assistants/_components/assistant-card.tsx
+++ src/app/(private)/(user)/assistants/_components/assistant-card.tsx
@@ -10,7 +10,8 @@ export const AssistantCard = ({ agent }: { agent: Agente }) => {
   const { userData } = useContext(SidebarContext);
 
   return (
-    <Card className="p-0 gap-0 relative overflow-hidden shadow-none">
+    <Card className="p-0 gap-0 relative overflow-hidden shadow-none 
+      hover:scale-105 transition-all cursor-pointer">
       <div className="w-full flex flex-col p-4">
         <Image
           src={`${process.env.NEXT_PUBLIC_S3_FILES}/assets/images/logo-dana.png`}
