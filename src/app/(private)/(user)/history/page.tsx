b1352ae835b4b4c2552dcdc8d4a277fc0629347e
Jean Carlos <lopesjean81@gmail.com>
2025-05-29 12:05:05 -0300

    Enhance HistoryPage component: replace plain text with structured JSX layout, including a title and grid for conversation history.

--- src/app/(private)/(user)/history/page.tsx
+++ src/app/(private)/(user)/history/page.tsx
@@ -1,3 +1,10 @@
 export default function HistoryPage() {
-    return "History"
+  return (
+    <div className="p-6 mt-5 bg-background rounded-xl border">
+      <h2 className="text-xl md:text-2xl font-semibold">
+        Histórico de Conversas
+      </h2>
+      <div className="px-2 md:px-6 grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 gap-6"></div>
+    </div>
+  );
 }
