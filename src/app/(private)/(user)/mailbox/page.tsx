9d8e563531fd1d55c1e765f97dea4626776be33d
Jean Carlos <lopesjean81@gmail.com>
2025-06-09 20:10:48 -0300

    Enhance mailbox functionality: add DataTable component for displaying payments, implement Checkbox for row selection, and update MailboxPage layout with message input and submission features.

--- src/app/(private)/(user)/mailbox/page.tsx
+++ src/app/(private)/(user)/mailbox/page.tsx
@@ -1,3 +1,23 @@
+import { Button } from "@/components/ui/button";
+import { Input } from "@/components/ui/input";
+import { columns, payments } from "@/features/mailbox/components/columns";
+import { DataTable } from "@/features/mailbox/components/data-table";
+import { SendHorizonal } from "lucide-react";
+
 export default function MailboxPage() {
-  return "Mailbox";
+  return (
+    <div className="p-6 mt-5 bg-background rounded-xl border">
+      <div className="px-2 md:px-6 grid grid-cols-1 gap-6">
+        <h2 className="text-2xl font-semibold">Mensagens</h2>
+        <DataTable data={payments} columns={columns} />
+        <div className="flex gap-2 w-full items-center">
+          <Input placeholder="Digite a mensagem aqui" />
+          <Button className="rounded-md">
+            Enviar
+            <SendHorizonal />
+          </Button>
+        </div>
+      </div>
+    </div>
+  );
 }
