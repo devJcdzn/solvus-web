1aa45fc9916c1b0d95ed798ef0e7f262639b10be
Jean Carlos <lopesjean81@gmail.com>
2025-06-13 17:58:43 -0300

    Refactor AssistantPage component: update rendering to display assistant ID within a styled layout for improved user experience.

--- src/app/(private)/(user)/assistants/[assistantId]/page.tsx
+++ src/app/(private)/(user)/assistants/[assistantId]/page.tsx
@@ -6,5 +6,9 @@ export default function AssistantPage() {
   const params = useParams();
   const assId = params.assistantId as string;
 
-  return assId;
+  return (
+    <div className="flex flex-col h-screen items-center justify-center max-w-5xl mx-auto px-6 md:px-0">
+      <div className="grid place-items-center mb-10">{assId}</div>
+    </div>
+  );
 }
